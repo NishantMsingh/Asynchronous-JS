6b2d6e0078acb8a0668221149e0ad41d85341ce5
Nishant Kumar <93445529+NishantMsingh@users.noreply.github.com>
2023-02-26 00:32:12 +0530

    html to js

--- createlogactivity.js
+++ createlogactivity.js
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Creation log activity</title>
-  </head>
-  <body>
-    <script>
+
       let posts = [{ title: "post 1" }];
 
       async function beforesctivity() {
@@ -73,6 +64,4 @@ beforesctivity()
   });
 
 
-</script>
-  </body>
-</html>
+
